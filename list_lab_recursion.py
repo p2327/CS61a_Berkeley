10fd788621c285e580e8fc8455ebfa77134368e8
osmya <45291785+osmya@users.noreply.github.com>
2019-06-19 15:25:32 +0100

    Update list_lab_recursion.py

--- list_lab_recursion.py
+++ list_lab_recursion.py
@@ -29,3 +29,29 @@ def merge(lst1, lst2):
         return [lst2[0]] + merge(lst1, lst2[1:])
         
 test2 = merge([1, 3, 5], [2, 4, 6])
+
+
+
+def merge_iter(lst1, lst2):
+    """Merges two sorted lists.
+    >>> merge_iter([1, 3, 5], [2, 4, 6])
+    [1, 2, 3, 4, 5, 6]
+    >>> merge_iter([], [2, 4, 6])
+    [2, 4, 6]
+    >>> merge_iter([1, 2, 3], [])
+    [1, 2, 3]
+    >>> merge_iter([5, 7], [2, 4, 6])
+    [2, 4, 5, 6, 7]
+    """
+    new = []
+    while lst1 and lst2:
+        if lst1[0] < lst2[0]:
+            new += [lst1[0]]
+            lst1 = lst1[1:]
+        else:
+            new += [lst2[0]]
+            lst2 = lst2[1:]
+    if lst1:
+        return new + lst1
+    else:
+        return new + lst2
