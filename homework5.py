518be2b15d9ae2b28f2f6763550200bf1acadc96
osmya <45291785+osmya@users.noreply.github.com>
2019-07-01 14:57:40 +0100

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -35,3 +35,49 @@ if password != pw:
                 return f'Your account is locked. Attempts: {unpack(wrongpw)}'
             return 'Wrong password'
             """
+
+
+# Question 2
+
+def make_withdraw(balance, password):
+    assert isinstance (password, str), 'Password is a string'
+    wrongpw = []
+    # with unpack helper function
+    def unpack(s):
+        return " ".join(map(str, s))
+    def withdraw(amount, p):
+        nonlocal wrongpw, password, balance
+        if p != password:
+            wrongpw += [p]
+            if len(wrongpw) >= 3:
+                return f'Your account is locked. Attempts: {unpack(wrongpw)}'
+            return 'Wrong password'
+        elif p == password:
+            if amount > balance:
+                return 'Insufficient funds'
+            balance = balance - amount
+            return balance
+    return withdraw
+        
+wd = make_withdraw(260000, 'maripos')
+
+""" wd(10, 'abdci')
+wd(10, 'aishie')
+wd(10, 'ldfw39u2')"""
+
+def make_joint(withdraw, oldpw, newpw):
+    account_test = withdraw(0, oldpw) # func stores newpw, oldpw values
+    if type(account_test) == str:
+        return account_test
+    def joint(amount, pw_attempt): # check if pw to withdrwa matches newpw
+        if pw_attempt == newpw:
+            return withdraw(amount, oldpw) # if true it calls withdrwa on the oldpw, enabling withdrawing
+        else:
+            return withdraw(amount, pw_attempt) # if not it calls withdraw on a the wrong password
+    return joint # returns a function joint
+
+
+j = make_joint(wd, 'maripos', 'holla')
+
+j(10, 'bogo')
+    
