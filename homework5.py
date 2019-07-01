047d125ee26ec7cdf2fe743e3bda2186558f963b
osmya <45291785+osmya@users.noreply.github.com>
2019-07-01 16:46:27 +0100

    Update homework5.py

--- homework5.py
+++ homework5.py
@@ -80,3 +80,76 @@ j = make_joint(wd, 'mck32nz1', 'h0lla')
 
 j(10, 'b6g8')
     
+##############
+###CLASSES####
+##############
+
+# Q1
+
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Machine is out of stock.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'You must deposit $10 more.'
+    >>> v.deposit(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'You must deposit $3 more.'
+    >>> v.deposit(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.deposit(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.deposit(15)
+    'Machine is out of stock. Here is your $15.'
+    """
+    "*** YOUR CODE HERE ***"
+    def __init__(self, vend_stock, candy_type, candy_price):
+        self.balance = 0
+        self.stock = vend_stock
+        self.candy = candy_type
+        self.price = candy_price
+    
+    def vend(self):
+        change = self.balance - self.price
+        def check(res):
+            if self.balance == 0:
+                return f'You must deposit ${self.price}'
+            elif res == 'yes':
+                return f"Here's your candy. Balance: ${self.balance}"
+            else:
+                self.balance = 0
+                return f"Here's your candy and ${change} change"
+        if self.stock == 0:
+            return 'Machine out of stock'
+        else:
+            if self.balance < 10:
+                return f'You must deposit ${10-self.balance} more'
+            elif change >= 0:
+                self.stock -= 1
+                self.balance -= self.price
+                if self.balance < 0:
+                    self.balance = 0
+                ans = input('Buy more?: yes/no')
+                return check(ans)
+    
+    def deposit(self, amount):
+        if self.stock == 0:
+            return f"Out of stock. Here's your ${amount}"
+        self.balance += amount
+        return f'Current balance: ${self.balance}'
+    
+    def restock(self, quantity):
+        self.stock += quantity
+        return f'Current {self.candy} stock: {self.stock}'
+    
+
+mm = VendingMachine(3, 'Mars bars', 10)
