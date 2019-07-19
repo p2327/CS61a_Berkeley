53e6562ba317f237e5e1e19814f08544c2392756
osmya <45291785+osmya@users.noreply.github.com>
2019-07-19 08:46:23 +0100

    Update discussion6.py

--- discussion6.py
+++ discussion6.py
@@ -52,3 +52,65 @@ ans = s1.take_all()
         print([s.color for s in out])
         return out
         """
+
+
+class Pet(object):
+    
+    def __init__(self, name, owner):
+        self.is_alive = True # why pass it into the init and not class?
+        self.name = name
+        self.owner = owner
+        
+    def __repr__(self):
+        return f"{self.name} is {self.owner}'s pet"
+    
+    def eat(self, thing):
+        print(f'{self.name} ate a {thing}!')
+        
+    def talk(self):
+        print('Woof!')
+        
+#####################
+#####################
+
+class Cat(Pet):
+    
+    def __init__(self, name, owner, lives=9):
+        Pet.__init__(self, name, owner)
+        self.lives = lives
+        
+    def talk(self):
+        print('Meow')
+        
+    def lose_life(self, num):
+        self.lives -= num
+        if self.lives <= 0:
+            self.is_alive = False
+            print(f'{self.name} lost all his lives')
+        return self.lives
+            
+    def _1up(self):
+        self.lives += 1
+        self.is_alive = True
+        return self.lives
+
+"""mr_waffles = Pet('Mr Waffles', 'Nicol')
+
+print(mr_waffles)
+
+mr_waffles.eat('cookie')
+
+mr_waffles.talk()"""
+
+tor = Cat('Thor', 'Jona')
+
+
+tor.talk()
+
+test = tor.is_alive
+
+tor.lose_life(9)
+
+tor._1up()
+
+test = tor.is_alive
